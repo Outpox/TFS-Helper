88cde7f7d6a7ec6feb6479b47b70158eef920e91
unknown <gum@WK-JXT-06.juxta.fr>
2015-09-25 10:58:22 +0200

    Initial commit

---
+++ handleDescription.js
@@ -0,0 +1,37 @@
+$(function() {
+    //Obligé d'attendre le chargement de la page avec un timeout car les éléments sont chargés après le chargement de la page
+    setTimeout(function(){
+        displayElements();
+        inject();
+    }, 2000);
+});
+
+function inject() {
+    console.info("Injecting...");
+    var leftHubContentLinks = $(".query-folder-tree.tree-view");
+    $(leftHubContentLinks).on("click", function() {
+        setTimeout(function(){
+            displayElements();
+        }, 250);
+        //Timer pour attendre le chargement des nouvelles tâches dans le DOM
+    });
+}
+
+function displayElements() {
+    console.info("Description events loaded...");
+    var el = $(".query-result-grid > .grid-canvas > .grid-row-normal");
+    for (var i = 0; i < el.length; i++) {
+        $(el[i]).on("click", function(){
+            displayIframe();
+        });
+    }
+}
+
+function displayIframe() {
+	var content = window.open("", "content", "width=1000, height=800");
+    var rightPane = $("div.rightPane.hub-no-content-gutter");
+    var iframes = rightPane.find("iframe");
+    var description = iframes[0];
+    var toInsert = description.contentWindow.document.body.innerHTML;;
+    $(content.document.body).append(toInsert);
+}
